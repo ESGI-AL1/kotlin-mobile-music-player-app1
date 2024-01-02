61870fc0a4176c3551d0b60f403dcfa471f5e11d
catri-01 <catrimaouene@gmail.com>
2024-01-02 16:33:33 +0100

    add structure of layout

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,7 +41,13 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    //implementation("androidx.navigation:navigation-fragment-ktx:2.5.5")
+    implementation("androidx.navigation:navigation-fragment:2.7.6")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+
+    //image animation
+    implementation("com.airbnb.android:lottie:3.7.1")
 }
